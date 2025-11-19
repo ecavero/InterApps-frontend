8126e9c2a34d91798c8e0494672e2c955881e781
Eduardo Cavero <ecavero@gmail.com>
2025-11-19 18:12:05 -0500

    Agregar el token.

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,8 +1,21 @@
 import { Link, Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
 import NavigationTabs from "../components/NavigationTabs";
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "../api/DevTreeAPI";
 
 export default function AppLayout() {
+    const {data, isLoading, error, isError} = useQuery({
+            queryFn: getUser,
+            queryKey: ['user'],
+            retry: 1,
+            refetchOnWindowFocus: false
+    })
+
+    console.log(data)
+    console.log(isLoading)
+    console.log(error)
+    console.log(isError)
 
 
     return (
