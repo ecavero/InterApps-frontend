8a9500cce7b1582452d3726208dd78428e136bb3
Eduardo Cavero <ecavero@gmail.com>
2025-11-19 14:46:44 -0500

    ReactQuery instalado y configurado.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './router'
 import './index.css'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router/>  
+    <QueryClientProvider client={queryClient}>
+            <Router/>  
+            <ReactQueryDevtools/>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
