ca3e19f2ed1509e38cec4232170b0fd6b6505c0b
Eduardo Cavero <ecavero@gmail.com>
2025-11-19 22:08:53 -0500

    Agregando validación del nombre de usuario.

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -1,9 +1,16 @@
+import { useForm } from "react-hook-form"
+import ErrorMessage from "../components/ErrorMessage"
+
 export default function ProfileView() {
 
+    const {register, handleSubmit, formState: {errors}} = useForm({defaultValues: {handle: ''}})
+    const handleUserProfileForm = () => {
+            console.log('desde handleUserProfileForm')
+    }
     return (
         <form 
             className="bg-white p-10 rounded-lg space-y-5"
-            onSubmit={() => {}}
+            onSubmit={handleSubmit(handleUserProfileForm)}
         >
             <legend className="text-2xl text-slate-800 text-center">Editar Información</legend>
             <div className="grid grid-cols-1 gap-2">
@@ -14,7 +21,11 @@ export default function ProfileView() {
                     type="text"
                     className="border-none bg-slate-100 rounded-lg p-2"
                     placeholder="handle o Nombre de Usuario"
+                    {...register('handle', {
+                            required: "El nombre de usuario es obligatorio"
+                        })}
                 />
+                {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
             </div>
 
             <div className="grid grid-cols-1 gap-2">
@@ -48,4 +59,5 @@ export default function ProfileView() {
             />
         </form>
     )
+
 }
