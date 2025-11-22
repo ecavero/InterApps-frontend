0aa9c6a892e967a7ebdc973a5507358b5894e174
andrea <andreamelissa.tc@gmail.com>
2025-11-22 17:55:06 -0500

    avance pag3

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 //creamos una instancia de axios
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+        baseURL: import.meta.env.VITE_API_URL
 })
 
 api.interceptors.request.use((config) => {
